5ec26e29b74f3fb4fe84cb37be32ee1a4f6ae81f
EmmaSyu <emma831215@gmail.com>
2023-01-03 19:24:51 +0800

    增加導覽路由、測試路由屬性與方法

--- src/views/NewPage.vue
+++ src/views/NewPage.vue
@@ -14,6 +14,15 @@
         <router-link to="/newpage/namedView/a2b" class="list-group-item list-group-item-action" aria-current="true">
           命名路由 a2b
         </router-link>
+        <router-link to="/newpage/dynamicRouter/55061c0fa1fcddb7" class="list-group-item list-group-item-action" aria-current="true">
+          動態路由 ($route)
+        </router-link>
+        <router-link to="/newpage/dynamicRouterByProps/55061c0fa1fcddb7" class="list-group-item list-group-item-action" aria-current="true">
+          動態路由 (props)
+        </router-link>
+        <router-link to="/newpage/routerNavigation" class="list-group-item list-group-item-action" aria-current="true">
+          路由導覽
+        </router-link>
       </div>
     </div>
     <div class="col-8">
